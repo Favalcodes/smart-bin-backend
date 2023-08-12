56d17833af0f2181f1cecb6240308e0df6814c37
Favour Arua <favourvalg@gmail.com>
2023-08-12 23:05:11 +0100

    moved index.js

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const errorHandler = require('./api/middleware/errorHandler')
+const connectDb = require('./api/config/database')
+require('dotenv').config()
+
+const app = express()
+connectDb(process.env.MONGO_URL)
+
+app.use(bodyParser.json({type: 'application/json'}))
+app.get('/', (req, res) => {
+    res.send('Server is running')
+})
+app.use('/api/user', require('./api/routes/userRoutes'))
+app.use('/api/restaurant', require('./api/routes/restaurantRoutes'))
+app.use('/api/guest', require('./api/routes/guestRoutes'))
+app.use('/api/review', require('./api/routes/reviewRoutes'))
+app.use('/api/menu', require('./api/routes/menuRoutes'))
+app.use('/api/admin', require('./api/routes/adminRoutes'))
+app.use(errorHandler)
+
+app.listen(process.env.PORT, () => {
+    console.log('Server started on 4000')
+})
+
+module.exports = app
